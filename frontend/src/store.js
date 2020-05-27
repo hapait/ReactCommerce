0ea3f95653888cc570425c5da8357ecf54948f7e
hapait <s.jamil1993@gmail.com>
2020-05-27 22:14:57 +0600

    redux functionality added to home page

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {productListReducer} from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
+const composeEnhancer = window.__RDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+export default store;
